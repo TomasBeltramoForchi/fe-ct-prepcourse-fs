5af19f44a2978a6ed4c744ca3fd41373cc642afe
Tomas <tomasbeltramoforchi@gmail.com>
2023-08-04 00:19:54 -0300

    modulosHastaEl03

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -5,36 +5,55 @@ function sonIguales(x, y) {
    // Retorna true si "x" e "y" son iguales.
    // De lo contrario, retorna false.
    // Tu código:
+   var resultado = x === y;
+    return resultado;
 }
 
 function tienenMismaLongitud(str1, str2) {
    // Retorna true si los dos strings tienen la misma longitud.
    // De lo contrario, retorna false.
    // Tu código:
+   if(str1.length === str2.length) {
+      var retorno = true;
+  } else {
+      var retorno = false; 
+  }
+  return retorno;
 }
+
 
 function menosQueNoventa(num) {
    // Retorna true si el argumento "num" es menor que noventa.
    // De lo contrario, retorna false.
    // Tu código:
+   if(num < 90) {
+      return true;
+    } else {
+        return false;
+    }
 }
 
 function mayorQueCincuenta(num) {
    // Retorna true si el argumento "num" es mayor que cincuenta.
    // De lo contrario, retorna false.
    // Tu código:
+   var respuesta = num > 50;
+   return respuesta;
 }
 
 function esPar(num) {
    // Retorna true si "num" es par.
    // De lo contrario, retorna false.
    // Tu código:
+   
+   return num % 2 === 0;
 }
 
 function esImpar(num) {
    // Retorna true si "num" es impar.
    // De lo contrario, retorna false.
    // Tu código:
+   return num % 1 !== 0;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -8,46 +8,67 @@ function esPositivo(num) {
    // Si el número es negativo ---> "Es negativo".
    // Si el número es 0, devuelve false.
    // Tu código:
+   if (num == 0){
+      var retorno = false;
+   } else if (num >= 1) {
+      var retorno = "Es positivo";
+   } else {
+      var retorno = "Es negativo";
+   }
+   return retorno;
 }
 
 function agregarSimboloExclamacion(str) {
    // Agrega un símbolo de exclamación al final del string "str" y retórnalo
    // Ejemplo: "hello world" ---> "hello world!"
    // Tu código:
+   return str + "!"
 }
 
 function combinarNombres(nombre, apellido) {
    // Retorna "nombre" y "apellido" combinados en un mismo string pero separados por un espacio.
    // Ejemplo: ("Soy", "Henry") ---> "Soy Henry"
    // Tu código:
+   var nombreCompleto = nombre + " " + apellido;
+   return nombreCompleto;
 }
 
 function obtenerSaludo(nombre) {
    // Toma el string "nombre" y concatena otra string en la cadena para que tome la siguiente forma:
    // Ejemplo: "Martin" ---> "Hola Martin!"
    // Tu código:
+   var saludar = "Hola" + " " + nombre +"!";
+   return saludar;
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
    // Retornar el área de un rectángulo teniendo su altura y ancho.
    // Tu código:
+   var area = alto * ancho;
+   return area;
 }
 
 function retornarPerimetro(lado) {
    // La función recibe como argumento la medida de un lado de un cuadrado.
    // Debes retornar su perímetro.
    // Tu código:
+   var perimetro = lado * 4;
+   return perimetro;
 }
 
 function areaDelTriangulo(base, altura) {
    // Calcula el área de un triángulo y retorna el resultado.
    // Tu código:
+   var area = (base * altura) / 2;
+   return area;
 }
 
 function deEuroAdolar(euro) {
    // Supongamos que 1 euro equivale a 1.20 dólares.
    // Debes calcular el valor recibido como argumento pasándolo a dolares.
    // Tu código:
+   var dinero = euro * 1.20 + " " + "dólares";
+   return dinero;
 }
 
 function esVocal(letra) {
@@ -55,6 +76,22 @@ function esVocal(letra) {
    // Si el usuario ingresa un string de más de un caracter debes retornar el mensaje: "Dato incorrecto".
    // Si no es vocal, tambien debe retornar "Dato incorrecto".
    // Tu código:
+   if (letra == "a") {
+      var retorno = "Es vocal";
+   } else if (letra == "e") {
+      var retorno = "Es vocal";
+   } else if (letra == "i") { 
+      var retorno = "Es vocal";
+   } else if (letra == "o") {
+      var retorno = "Es vocal";
+   } else if (letra == "u") {
+      var retorno = "Es vocal";
+   } else if (letra.length > 1) {
+      var retorno = "Dato incorrecto";
+   } else {
+      var retorno = "Dato incorrecto";
+   }
+   return retorno;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
